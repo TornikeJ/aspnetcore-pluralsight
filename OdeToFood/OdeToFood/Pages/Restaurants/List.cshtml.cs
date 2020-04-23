655c39d32ab1987870a305be0d678d413bf2466b
TornikeJ <t.janashia11@gmail.com>
2020-04-23 21:23:32 +0400

    finish first module

---
+++ OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Data;
+using OdeToFood.Core;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class ListModel : PageModel
+    {
+        public string Message { get; set; }
+        public IEnumerable<OdeToFood.Core.Restaurant> Restaurants { get; set; }
+
+        private readonly IRestaurantData restaurantData;
+
+        public ListModel(IRestaurantData restaurantData)
+        {
+            this.restaurantData = restaurantData;
+        }
+        public void OnGet()
+        {
+            Message = "Hello World";
+            Restaurants = restaurantData.GetAll();
+        }
+    }
+}
