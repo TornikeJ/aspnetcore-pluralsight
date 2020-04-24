838840bb8c9dfa563d05f12458baa2a80fea1015
TornikeJ <t.janashia11@gmail.com>
2020-04-24 12:20:49 +0400

    finish third module

--- OdeToFood/OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood/OdeToFood.Data/IRestaurantData.cs
@@ -10,6 +10,10 @@ namespace OdeToFood.Data
     {
         IEnumerable<Restaurant> GetRestarantsByName(string name);
         Restaurant GetRestaurantById(int id);
+        Restaurant UpdateRestaurant(Restaurant updatedRestaurant);
+
+        Restaurant AddNewRestaurant(Restaurant newRestaurant);
+        int Commit();
     }
 
     public class InMemoryRestaurantData : IRestaurantData
@@ -36,6 +40,32 @@ namespace OdeToFood.Data
         public Restaurant GetRestaurantById(int id)
         {
             return restaurants.SingleOrDefault(r => r.Id == id);
+        }
+
+        public Restaurant UpdateRestaurant(Restaurant updatedRestaurant)
+        {
+            var restaurant = restaurants.SingleOrDefault(r => r.Id == updatedRestaurant.Id);
+            if(restaurant != null)
+            {
+                restaurant.Name = updatedRestaurant.Name;
+                restaurant.Location = updatedRestaurant.Location;
+                restaurant.Cuisine = updatedRestaurant.Cuisine;
+
+            }
+
+            return restaurant;
+        }
+
+        public Restaurant AddNewRestaurant(Restaurant newRestaurant)
+        {
+            restaurants.Add(newRestaurant);
+            newRestaurant.Id = restaurants.Max(r => r.Id) + 1;
+            return newRestaurant;
+        }
+
+        public int Commit()
+        {
+            return 0;
         }
     }
 }
