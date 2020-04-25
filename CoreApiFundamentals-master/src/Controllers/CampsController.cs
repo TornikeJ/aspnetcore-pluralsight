c9ff52f85ba6e8735361812171fcf686b7fc4c1f
TornikeJ <t.janashia11@gmail.com>
2020-04-25 14:17:57 +0400

    api-finish module  (building api)

---
+++ CoreApiFundamentals-master/src/Controllers/CampsController.cs
@@ -0,0 +1,76 @@
+﻿using AutoMapper;
+using CoreCodeCamp.Data;
+using CoreCodeCamp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    public class CampsController : ControllerBase
+    {
+        private readonly ICampRepository repository;
+        private readonly IMapper mapper;
+
+        public CampsController(ICampRepository repository, IMapper mapper)
+        {
+            this.repository = repository;
+            this.mapper = mapper;
+        }
+        [HttpGet]
+        public async Task<ActionResult<CampModel[]>> GetCamps(bool includeTalks = false)
+        {
+            try
+            {
+                var results = await repository.GetAllCampsAsync(includeTalks);
+
+
+                return mapper.Map<CampModel[]>(results); ;
+
+            }
+
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database error");
+            }
+        }
+
+        [HttpGet("{moniker}")]
+        public async Task<ActionResult<CampModel>> GetCamp(string moniker)
+        {
+            try
+            {
+                var result = await repository.GetCampAsync(moniker);
+
+                if (result == null) return NotFound();
+
+                return mapper.Map<CampModel>(result);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database error");
+            }
+        }
+
+        [HttpGet("search")]
+        public async Task<ActionResult<CampModel[]>> SearchByDate(DateTime theDate, bool includeTalks=false)
+        {
+            try
+            {
+                var results = await repository.GetAllCampsByEventDate(theDate, includeTalks);
+
+                if (!results.Any()) return NotFound();
+
+                return mapper.Map<CampModel[]>(results);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database error");
+            }
+        }
+    }
+}
